63cbb4b4c6a6190e04a8a2df5b5fc76f68debf52
Jonatan1991 <75802929+Jonatan1991@users.noreply.github.com>
2026-03-22 16:17:08 +0100

    Agregar nuevo contacto "Roberto Casa Mendez" a la base de datos y actualizar la tabla de contactos para incluir el campo "Escalera" en la interfaz gráfica.

--- views/gui/tabla_contactos.py
+++ views/gui/tabla_contactos.py
@@ -6,9 +6,9 @@ class TablaContactos(QTableWidget):
     def __init__(self):
         super().__init__()
 
-        self.setColumnCount(12)
+        self.setColumnCount(13)
         self.setHorizontalHeaderLabels(
-            ["No.", "Nombre", "Apellido 1", "Apellido 2", "Teléfono", "Email", "Calle", "Número", "Piso", "Puerta", "CP", "Provincia"]
+            ["No.", "Nombre", "Apellido 1", "Apellido 2", "Teléfono", "Email", "Calle", "Número", "Piso", "Puerta", "Escalera", "CP", "Provincia"]
         )
 
         header = self.horizontalHeader()
@@ -23,17 +23,18 @@ class TablaContactos(QTableWidget):
 
         for fila, (id_contacto, datos) in enumerate(contactos.items()):
 
+            direccion = datos.get("direccion", {})
+
             self.setItem(fila, 0, QTableWidgetItem(id_contacto))
             self.setItem(fila, 1, QTableWidgetItem(datos.get("nombre", "")))
             self.setItem(fila, 2, QTableWidgetItem(datos.get("apellido_1", "")))
             self.setItem(fila, 3, QTableWidgetItem(datos.get("apellido_2", "")))
             self.setItem(fila, 4, QTableWidgetItem(datos.get("telefono", "")))
             self.setItem(fila, 5, QTableWidgetItem(datos.get("email", "")))
-
-            direccion = datos.get("direccion", {})
             self.setItem(fila, 6, QTableWidgetItem(direccion.get("calle", "")))
             self.setItem(fila, 7, QTableWidgetItem(direccion.get("numero", "")))
             self.setItem(fila, 8, QTableWidgetItem(direccion.get("piso", "")))
             self.setItem(fila, 9, QTableWidgetItem(direccion.get("puerta", "")))
-            self.setItem(fila, 10, QTableWidgetItem(direccion.get("cp", "")))
-            self.setItem(fila, 11, QTableWidgetItem(direccion.get("provincia", "")))
+            self.setItem(fila, 10, QTableWidgetItem(direccion.get("escalera", "")))
+            self.setItem(fila, 11, QTableWidgetItem(direccion.get("cp", "")))
+            self.setItem(fila, 12, QTableWidgetItem(direccion.get("provincia", "")))
